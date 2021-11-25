3505a1b7a1e8ed0c6b4b292b9d818ecf03cc0b22
Pace Willisson <pace@alum.mit.edu>
2021-11-25 18:22:10 -0500

    cleanup

--- encoder/encoder.v
+++ encoder/encoder.v
@@ -12,40 +12,62 @@ module top (
    wire 	   clk;
    wire 	   locked;
    
-   pll pll_inst (.clock_in(OSCI), .clock_out(clk), .locked(locked));
+   pll pll_inst(.clock_in(OSCI), .clock_out(clk), .locked(locked));
 
-   localparam SYS_CNTR_WIDTH = 24;
+   parameter decode1_width = 4;
+   wire [decode1_width-1:0] 	   count1;
+   quadrature_decode decode1 
+     (
+      .A(ENC_A_raw),
+      .B(ENC_B_raw),
+      .count(count1),
+      .clk(clk)
+      );
+   defparam decode1.width = decode1_width;
+   
+   
+   localparam SYS_CNTR_WIDTH = 25;
    reg [SYS_CNTR_WIDTH-1:0] syscounter;
 
-   reg [7:0] 		    count;
-   reg [1:0] 		    ENC_last;
-   
-   reg [2:0] 		    ENC_A_sync, ENC_B_sync;
-
-   always @(posedge clk) ENC_A_sync <= {ENC_A_sync[1:0], ENC_A_raw};
-   always @(posedge clk) ENC_B_sync <= {ENC_B_sync[1:0], ENC_B_raw};
-
-   wire [1:0] ENC_next = {ENC_A_sync[2], ENC_B_sync[2]};
-   
-   wire       count_direction = ENC_next[0] ^ ENC_last[1];
-   wire       count_enable =    ENC_next[1] ^ ENC_last[0] ^ count_direction;
-   
-   
-   always @(posedge clk) begin
-      syscounter <= syscounter + 1;
-
-      if (count_enable)
-	 count <= count_direction ? count + 1 : count - 1;
-
-      ENC_last <= ENC_next;
-      
-   end
+   always @(posedge clk) syscounter <= syscounter + 1;
 
    assign LED5 = syscounter[SYS_CNTR_WIDTH-1];
 
-   assign LED4 = count[3] == 0 && count[2] == 0;
-   assign LED3 = count[3] == 0 && count[2] == 1;
-   assign LED2 = count[3] == 1 && count[2] == 0;
-   assign LED1 = count[3] == 1 && count[2] == 1;
+   assign LED4 = count1[3] == 0 && count1[2] == 0;
+   assign LED3 = count1[3] == 0 && count1[2] == 1;
+   assign LED2 = count1[3] == 1 && count1[2] == 0;
+   assign LED1 = count1[3] == 1 && count1[2] == 1;
 
 endmodule		 
+
+
+module quadrature_decode(
+			 input 	A,
+			 input 	B,
+			 output [width-1:0] count,
+			 input 	clk);
+
+   parameter width = 8;
+   
+   reg [2:0] 			A_sync, B_sync;
+   always @(posedge clk) A_sync <= {A_sync[1:0], A};
+   always @(posedge clk) B_sync <= {B_sync[1:0], B};
+   wire [1:0] next = {A_sync[2], B_sync[2]};
+
+   reg [1:0] 			last;
+
+   reg [width-1:0] 		count_reg;
+   
+   wire       count_direction = next[0] ^ last[1];
+   wire       count_enable =    next[1] ^ last[0] ^ count_direction;
+   
+   always @(posedge clk) begin
+      if (count_enable)
+	 count_reg <= count_direction ? count + 1 : count - 1;
+
+      last <= next;
+   end
+
+   assign count = count_reg;
+
+endmodule // quadrature_decode
