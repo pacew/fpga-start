0937128b1a8d81416ba6a69c80d39af49acedeee
Pace Willisson <pace@alum.mit.edu>
2021-11-25 17:58:33 -0500

    cleanup

--- encoder/encoder.v
+++ encoder/encoder.v
@@ -17,7 +17,7 @@ module top (
    localparam SYS_CNTR_WIDTH = 24;
    reg [SYS_CNTR_WIDTH-1:0] syscounter;
 
-   reg [7:0] 		    position;
+   reg [7:0] 		    count;
    reg [1:0] 		    ENC_last;
    
    reg [2:0] 		    ENC_A_sync, ENC_B_sync;
@@ -27,39 +27,15 @@ module top (
 
    wire [1:0] ENC_next = {ENC_A_sync[2], ENC_B_sync[2]};
    
+   wire       count_direction = ENC_next[0] ^ ENC_last[1];
+   wire       count_enable =    ENC_next[1] ^ ENC_last[0] ^ count_direction;
+   
+   
    always @(posedge clk) begin
-     syscounter <= syscounter + 1;
+      syscounter <= syscounter + 1;
 
-      case (ENC_last)
-	2'b00: 
-	  begin
-	     if (ENC_next[0] == 1)
-	       position <= position + 1;
-	     else if (ENC_next[1] == 1)
-	       position <= position - 1;
-	  end
-	2'b01:
-	  begin
-	     if (ENC_next[1] == 1)
-	       position <= position + 1;
-	     else if (ENC_next[0] == 0)
-	       position <= position - 1;
-	  end 
-	2'b11:
-	  begin
-	     if (ENC_next[0] == 0)
-		position <= position + 1;
-	     else if (ENC_next[1] == 0)
-		position <= position - 1;
-	  end 
-	2'b10:
-	  begin
-	     if (ENC_next[1] == 0)
-		position <= position + 1;
-	     else if (ENC_next[0] == 1)
-		position <= position - 1;
-	  end
-      endcase
+      if (count_enable)
+	 count <= count_direction ? count + 1 : count - 1;
 
       ENC_last <= ENC_next;
       
@@ -67,9 +43,9 @@ module top (
 
    assign LED5 = syscounter[SYS_CNTR_WIDTH-1];
 
-   assign LED4 = position[3] == 0 && position[2] == 0;
-   assign LED3 = position[3] == 0 && position[2] == 1;
-   assign LED2 = position[3] == 1 && position[2] == 0;
-   assign LED1 = position[3] == 1 && position[2] == 1;
+   assign LED4 = count[3] == 0 && count[2] == 0;
+   assign LED3 = count[3] == 0 && count[2] == 1;
+   assign LED2 = count[3] == 1 && count[2] == 0;
+   assign LED1 = count[3] == 1 && count[2] == 1;
 
 endmodule		 
