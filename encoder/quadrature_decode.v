fc810588b6eb5d7a042ac3fdccb3d32b006c5330
Pace Willisson <pace@alum.mit.edu>
2021-11-25 22:48:49 -0500

    sim

--- encoder/quadrature_decode.v
+++ encoder/quadrature_decode.v
@@ -1,30 +1,25 @@
 module quadrature_decode(
 			 input 	A,
 			 input 	B,
-			 output [width-1:0] count,
+			 output reg [width-1:0] count = 0,
 			 input 	clk);
-
    parameter width = 8;
    
-   reg [2:0] 			A_sync, B_sync;
+   reg [2:0] 			A_sync = 1'b0, B_sync = 1'b0;
    always @(posedge clk) A_sync <= {A_sync[1:0], A};
    always @(posedge clk) B_sync <= {B_sync[1:0], B};
    wire [1:0] next = {A_sync[2], B_sync[2]};
 
-   reg [1:0] 			last;
+   reg [1:0] 			last = 0;
 
-   reg [width-1:0] 		count_reg;
-   
    wire       count_direction = next[0] ^ last[1];
    wire       count_enable =    next[1] ^ last[0] ^ count_direction;
    
    always @(posedge clk) begin
       if (count_enable)
-	 count_reg <= count_direction ? count + 1 : count - 1;
+	 count <= count_direction ? count + 1 : count - 1;
 
       last <= next;
    end
-
-   assign count = count_reg;
 
 endmodule // quadrature_decode
---
+++ encoder/quadrature_decode_tb.v
@@ -0,0 +1,71 @@
+`timescale 1ns / 1ns
+
+`include "quadrature_decode.v"
+
+`define ASSERT(exp, message) \
+     begin if (!(exp)) begin \
+	UT_FAIL = UT_FAIL + 1; \
+		  $display("unit test error: ", message); end end
+
+module test;
+   integer UT_FAIL = 0;
+   
+
+   reg 	   clk = 1;
+   always #5 clk = !clk;
+   
+   
+   reg A = 1'bx, B = 1'bx;
+   
+   parameter width = 8;
+   wire [width-1:0] count;
+   
+   quadrature_decode uut(A, B, count, clk);
+   defparam uut.width = width;
+
+  initial begin
+     $dumpfile("sim.vcd");
+     $dumpvars;
+     
+     A = 0; B = 0;
+     #10;
+     `ASSERT(count == 0, "initially zero");
+     
+     A = 0; B = 1;
+     #30;
+     `ASSERT(count == 0, "still 0");
+     #10;
+     `ASSERT(count == 1, "counted to 1");
+
+     A = 1; B = 1;
+     #40;
+     `ASSERT(count == 2, "counted to 2");
+
+     A = 1; B = 0;
+     #40;
+     `ASSERT(count == 3, "counted to 3");
+
+     A = 0; B = 0;
+     #40;
+     `ASSERT(count == 4, "counted to 4");
+
+     A = 1; B = 0;
+     #40;     
+     `ASSERT(count == 3, "counted to back to 3");
+
+     A = 0; B = 0;
+     #40;
+     `ASSERT(count == 4, "counted to back to 4");
+
+
+     if (UT_FAIL) begin
+	$display("error: ", UT_FAIL, " unit tests failed");
+     end else begin
+	$display("unit tests: success");
+     end
+     $finish;
+     
+     
+  end // initial begin
+   
+endmodule // test
